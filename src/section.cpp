bb16f2ce9a606e972feda196d50a74e21ed2ca94
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-04-02 00:31:56 +0300

    convex polygon intersection algorithm from O'Rourke is pure shit

--- src/section.cpp
+++ src/section.cpp
@@ -2,51 +2,8 @@
 
 namespace sect
 {
-	using vec2 = prim::vec2;
-
-	using Line2 = prim::Line2;
-
-	// for now only two segments can intersect in the same point
-	struct Event
+	void sect_n_lines(std::vector<prim::Line2>& lines)
 	{
-		enum class Type : i32
-		{
-			Lower,
-			Intersection,
-			Upper
-		};
-		
-		std::vector<Line2*> segments;
-		vec2* pt{};
-		Type type{};
-	};
-
-	struct Intersection
-	{
-		std::vector<Line2*> intersection;
-	};
-
-	class SweepLine
-	{
-	private:
-	};
-
-	// stores events
-	class EventQueue
-	{
-	public:
-		void extractMin()
-		{
-			// TODO
-		}
-
-	private:
-		// 
-	};
-
-
-	void section_n_lines(std::vector<prim::Line2>& lines)
-	{
-	
+		// TODO
 	}
 }
