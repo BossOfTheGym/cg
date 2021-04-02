42fbb3d729e1dabd0e494cbd8739053d9d9cb23f
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-04-02 17:00:45 +0300

    primitive test files

---
+++ src/primitive_test.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+struct LineTest;
+
+void test_intersections_seg_seg();
+
+void test_intersections_seg_line();
+
+
+struct LineTestP;
+
+void test_intersectionsX();
+
+void test_intersectionsY();
