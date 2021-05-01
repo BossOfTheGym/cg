d413262596844f6ed3cc103ad89cf2b5f3a4e790
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-05-01 21:39:48 +0300

    forgot not to add main into index(sigh... this must have been in project for tests)

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,12 @@
 #include <climits>
 #include <iostream>
 
-#include "hull_test.h"
-
 int main()
 {
-	test_hull();
-	
-	//App app;
-	//app.init();
-	//app.exec();
-	//app.deinit();
+	App app;
+	app.init();
+	app.exec();
+	app.deinit();
 
 	return 0;
 }
